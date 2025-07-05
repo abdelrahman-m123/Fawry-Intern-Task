a92267961985d2ef21a939442d0ee4f3ef8f98b9
Abdelrahman Mostafa Emad <123787144+abdelrahman-m123@users.noreply.github.com>
2025-07-05 21:22:32 +0300

    Delete src directory

--- src/DigitalProduct.java
+++
@@ -1,7 +0,0 @@
-public class DigitalProduct extends Product{
-    public static final String category = "Digital";
-
-    public DigitalProduct(int id, String name, double price, int quantity) {
-        super(id, name, price, quantity);
-    }
-}
